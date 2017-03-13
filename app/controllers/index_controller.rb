a3787bf28cba332af227aa540b996acebc4974ef
Matt McGee <matthew.m.mcgee@gmail.com>
2017-03-13 00:16:13 -0500

    js new user tutorial done

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,10 +7,9 @@ get '/' do
 end
 
 get '/global_search' do
-	search = params["q"]
+	search = params["q"].gsub(" ", '%')
 	people = User.where('LOWER(email) LIKE LOWER(?)', "%#{search}%")
 	lists = List.where('LOWER(name) LIKE LOWER(?)', "%#{search}%" )
 	response = search_response(people, lists)
-
   return response.to_json
 end
